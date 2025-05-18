4a5807ce3ce677be8d222a3b8c0e01e7269ee9c4
Leonardo <lfm.fiap@gmail.com>
2025-05-18 12:32:47 -0300

    Editando o tailwindcss.

--- frontend/src/routes/Sobre.jsx
+++ frontend/src/routes/Sobre.jsx
@@ -1,10 +1,18 @@
 function Sobre() {
   return (
     <div className="container mx-auto py-16 px-4 sm:px-6 lg:px-8 text-center">
-      <h2 className="text-4xl font-extrabold text-primary mb-8">Sobre Nós</h2>
-      <p className="text-lg text-gray-600 max-w-2xl mx-auto">Está procurando a melhor bicicleta elétrica para transformar sua rotina com mais mobilidade e praticidade? Aqui, você encontra a bike elétrica ideal, com tecnologia de ponta. Nossas bikes são as mais acessíveis e com qualidade de ponta, temos uma para cada tipo de pessoa e situação.
+      <h2 className="text-4xl text-green-500 font-extrabold text-primary mb-8">Sobre Nós</h2>
+      <p className="text-lg text-white max-w-2xl mx-auto">Mergulhe na aventura com a Games Store, sua parada definitiva para videogames épicos! Aqui, você encontra os maiores lançamentos, clássicos nostálgicos e acessórios de ponta para turbinar sua jogatina. De consoles de última geração a promoções imbatíveis, nossa paixão por jogos garante a melhor experiência para todos os níveis de jogadores. Visite a Games Store e liberte o herói dentro de você!
       </p>
-      <h2>Leonardo Fernandes Mesquita, RM:559623 | Marco Antonio Caires Freire, RM:559256 | Guilherme Augusto Caseiro, RM:559765</h2>
+    <div className="container mx-auto px-4 sm:px-6 lg:px-8 text-center">
+          <a href="https://github.com/leoGitFiap" className="text-sm text-blue-400 max-w-2xl mx-auto">Leonardo Fernandes Mesquita, RM:559623 | </a>
+    </div>
+    <div className="container mx-auto px-4 sm:px-6 lg:px-8 text-center">
+          <a href="https://github.com/MACF77" className="text-sm text-blue-400 max-w-2xl mx-auto">Marco Antonio Caires Freire, RM:559256 | </a>
+    </div>
+    <div className="container mx-auto px-4 sm:px-6 lg:px-8 text-center">
+          <a href="https://github.com/Guiitens2005" className="text-sm text-blue-400 max-w-2xl mx-auto">Guilherme Augusto Caseiro, RM:559765</a>
+    </div>
     </div>
   );
 }
