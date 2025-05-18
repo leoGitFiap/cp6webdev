4d55d7d2808e459229e0d3f0d881bb89283365e3
Leonardo <lfm.fiap@gmail.com>
2025-05-18 15:19:05 -0300

    Arrumando o Home.jsx

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
       <p className="text-lg text-white max-w-2xl mx-auto">Mergulhe na aventura com a Games Store, sua parada definitiva para videogames épicos! Aqui, você encontra os maiores lançamentos, clássicos nostálgicos e acessórios de ponta para turbinar sua jogatina. De consoles de última geração a promoções imbatíveis, nossa paixão por jogos garante a melhor experiência para todos os níveis de jogadores. Visite a Games Store e liberte o herói dentro de você!
       </p>
       <a
-        href="/bicicletas"
+        href="/games"
         className="mt-8 inline-block bg-primary text-white px-6 py-3 rounded-lg hover:bg-green-500 hover:text-black hover:font-semibold transition"
       >
         Ver Catálogo
